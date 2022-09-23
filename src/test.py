bf057d70aa49ca5669a563f5bc80d6f3293e7acb
Mads Stenhuus <mads.stenhuus@hpe.com>
2022-09-23 17:33:19 +0200

    First Python program

---
+++ src/test.py
@@ -0,0 +1,16 @@
+
+
+def fact(x):
+	if x == 0:
+		return 1
+	else:
+		return x * fact(x - 1)
+
+for i in range(0, 10):
+	print("Hallo:", i)
+
+print(fact(0))
+print(fact(1))
+print(fact(2))
+print(fact(10))
+
